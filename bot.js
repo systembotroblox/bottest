050d3df4653e7fcf58b19573794ad8068a3bc668
systembotroblox <32761646+systembotroblox@users.noreply.github.com>
2017-11-10 16:24:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,36 +23,36 @@ client.on("message", message => {
 	let prefix = "!";
 	
 	console.log(0)
-	if(!msg.content.startsWith(prefix)) return;
+	if(!message.content.startsWith(prefix)) return;
 	
 	console.log("help command")
-	if (msg.content.startsWith(prefix + "help")) {
-		msg.reply("Here are my commands: !help,")
+	if (message.content.startsWith(prefix + "help")) {
+		message.reply("Here are my commands: !help,")
 	}
 	
-	let userData = XP[msg.author.id];
+	let userData = XP[message.author.id];
 	if (!userData) userData = {XP: 0, level: 0};
 	
-	let userXP = XP[msg.author.id] ? XP[msg.author.id].XP : 10;
+	let userXP = XP[message.author.id] ? XP[message.author.id].XP : 10;
 	let curLevel = Math.floor(0.1 * Math.sqrt(userXP));
 	if (curLevel > userData.level) {
 		userData.level = curLevel;
-		msg.reply(`You have lvled ^ to lvl **${curLevel}**!`);
+		message.reply(`You have lvled ^ to lvl **${curLevel}**!`);
 	}
 	
 	console.log("level")
-	if (msg.content.startsWith(prefix + "level")) {
-		msg.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
+	if (message.content.startsWith(prefix + "level")) {
+		message.reply(`You are lvl ${userData.level}, with ${userData.XP} XP Right Now.`);
 	}
 	
-	if (!XP[msg.author.id]) XP[msg.author.id] = {XP: 0, level: 0}
+	if (!XP[message.author.id]) XP[message.author.id] = {XP: 0, level: 0}
 	
 	
 	
 	console.log("Example")
-	if (msg.content.startsWith(prefix + "killExample")) {
+	if (message.content.startsWith(prefix + "killExample")) {
 		userData.XP += 10
-		msg.channel.sendMessage(`${msg.author} has killed an Example!`)
+		message.channel.sendMessage(`${message.author} has killed an Example!`)
 	}
 	
 	console.log(XP)
